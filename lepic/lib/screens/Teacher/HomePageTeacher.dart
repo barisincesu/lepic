a667504f808b1b5a3be21f557b327e66e2631287
barisincesu <barisincesu@dreamgames.com>
2022-01-11 02:51:13 +0300

    Add argument.

--- lepic/lib/screens/Teacher/HomePageTeacher.dart
+++ lepic/lib/screens/Teacher/HomePageTeacher.dart
@@ -46,22 +46,16 @@ class _TeacherHomePage extends State<TeacherHomePage> {
   ];
   @override
   Widget build(BuildContext context) {
+    final todo = ModalRoute.of(context).settings.arguments;
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: const Text("Teacher Page"),
+        title: Text("Teacher Page $todo"),
       ),
       body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const Padding(padding: EdgeInsets.only(top: 10.0)),
-            Text(
-              'School Name : $_name',
-              textAlign: TextAlign.center,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            ),
             DropdownButton(
               value: _currentClass,
               isExpanded: true,
@@ -76,102 +70,6 @@ class _TeacherHomePage extends State<TeacherHomePage> {
                 setState(() {
                   _currentClass = newValue;
                 });
-              },
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size.fromHeight(
-                    40), // fromHeight use double.infinity as width and 40 is the height
-              ),
-              child: Text(
-                "Add texts in $_currentClass",
-                style: const TextStyle(color: Colors.white),
-              ),
-              onPressed: () {
-                //debugPrint("Butona tıklandı");
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AddTextToClassTeacher(),
-                      settings: RouteSettings(
-                        arguments: _currentClass,
-                      ),
-                    ));
-              },
-            ),
-            const Padding(padding: EdgeInsets.only(top: 10.0)),
-            Text("Texts In $_currentClass"),
-            ListView.builder(
-                shrinkWrap: true,
-                itemCount: textsInClass.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: const Icon(Icons.text_snippet, color: Colors.cyan),
-                    title: Text(textsInClass[index]),
-                    trailing: const Icon(
-                      Icons.edit,
-                      color: Colors.green,
-                    ),
-                  );
-                }),
-            Text("Students In $_currentClass"),
-            ListView.builder(
-                shrinkWrap: true,
-                itemCount: studentsInClass.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading:
-                        const Icon(Icons.people_rounded, color: Colors.teal),
-                    title: Text(studentsInClass[index]),
-                    trailing: const Icon(
-                      Icons.select_all_sharp,
-                      color: Colors.black,
-                    ),
-                    onTap: () => {
-                      currentName = studentsInClass[index],
-                      showAlertDialog(context)
-                    },
-                  );
-                }),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size.fromHeight(
-                    40), // fromHeight use double.infinity as width and 40 is the height
-              ),
-              child: const Text(
-                "See the selected student report",
-                style: TextStyle(color: Colors.white),
-              ),
-              onPressed: () {
-                if (currentName == "") {
-                  showAlertDialog(context);
-                } else {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ReportIndivucialTeacher(),
-                        settings: RouteSettings(
-                          arguments: currentName,
-                        ),
-                      ));
-                }
-              },
-            ),
-            const Padding(padding: EdgeInsets.only(top: 10.0)),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size.fromHeight(
-                    40), // fromHeight use double.infinity as width and 40 is the height
-              ),
-              child: const Text(
-                "See all students reports",
-                style: TextStyle(color: Colors.white),
-              ),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => AllReportTeacher()));
               },
             ),
           ],
