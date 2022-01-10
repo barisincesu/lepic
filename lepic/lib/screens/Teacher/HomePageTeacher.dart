c3941d63e73d08b472397307958b2270248af666
barisincesu <barisincesu@dreamgames.com>
2022-01-11 02:43:33 +0300

    Fix DropdownButton.

--- lepic/lib/screens/Teacher/HomePageTeacher.dart
+++ lepic/lib/screens/Teacher/HomePageTeacher.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:lepic/screens/Teacher/AddTextToClass.dart';
 import 'package:lepic/screens/Teacher/AllReport.dart';
@@ -33,9 +32,9 @@ String currentName = "";
 
 class _TeacherHomePage extends State<TeacherHomePage> {
   final String _name = "Vefa Lisesi"; // Veritabanından çek !
-  String _currentClass = "6A"; // Comboda aktif olanı seç
+  String _currentClass = '10/A'; // Comboda aktif olanı seç
 
-  //final classes = ["10/A", "11/B", "12/C", "12/A"];
+  final classes = ["10/A", "11/B", "12/C", "12/A"];
 
   final List<String> studentsInClass = <String>[
     "Bariş İncesu",
@@ -45,163 +44,138 @@ class _TeacherHomePage extends State<TeacherHomePage> {
     "Text 1",
     "Text 2",
   ];
-
-  /*Future getDocs(String query, classes) async {
-    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-        .collection("products")
-        .where('teacherName', isEqualTo: query)
-        .get();
-    //classes.clear();
-    for (int i = 0; i < querySnapshot.docs.length; i++) {
-      var a = querySnapshot.docs[i];
-      classes.add(a.get('name'));
-      print(a.get('name'));
-    }
-  }*/
-
   @override
   Widget build(BuildContext context) {
-    final todo = ModalRoute.of(context).settings.arguments;
-    //getDocs(todo, this.classes);
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: Text("todo"),
+        title: const Text("Teacher Page"),
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('products')
-            .where('teacherName', isEqualTo: todo)
-            .snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          return Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const Padding(padding: EdgeInsets.only(top: 10.0)),
-                Text(
-                  'School Name : $_name',
-                  textAlign: TextAlign.center,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(fontWeight: FontWeight.bold),
-                ),
-                DropdownButton(
-                  value: _currentClass,
-                  isExpanded: true,
-                  icon: const Icon(Icons.keyboard_arrow_down),
-                  items: snapshot.data.docs.map((document) {
-                    return DropdownMenuItem(
-                      value: items,
-                      child: Text(items),
-                    );
-                  }).toList(),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size.fromHeight(
-                        40), // fromHeight use double.infinity as width and 40 is the height
-                  ),
-                  child: Text(
-                    "Add texts in $_currentClass",
-                    style: const TextStyle(color: Colors.white),
-                  ),
-                  onPressed: () {
-                    //debugPrint("Butona tıklandı");
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => AddTextToClassTeacher(),
-                          settings: RouteSettings(
-                            arguments: _currentClass,
-                          ),
-                        ));
-                  },
-                ),
-                const Padding(padding: EdgeInsets.only(top: 10.0)),
-                Text("Texts In $_currentClass"),
-                ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: textsInClass.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        leading:
-                            const Icon(Icons.text_snippet, color: Colors.cyan),
-                        title: Text(textsInClass[index]),
-                        trailing: const Icon(
-                          Icons.edit,
-                          color: Colors.green,
-                        ),
-                      );
-                    }),
-                Text("Students In $_currentClass"),
-                ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: studentsInClass.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        leading: const Icon(Icons.people_rounded,
-                            color: Colors.teal),
-                        title: Text(studentsInClass[index]),
-                        trailing: const Icon(
-                          Icons.select_all_sharp,
-                          color: Colors.black,
-                        ),
-                        onTap: () => {
-                          currentName = studentsInClass[index],
-                          showAlertDialog(context)
-                        },
-                      );
-                    }),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size.fromHeight(
-                        40), // fromHeight use double.infinity as width and 40 is the height
-                  ),
-                  child: const Text(
-                    "See the selected student report",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: () {
-                    if (currentName == "") {
-                      showAlertDialog(context);
-                    } else {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ReportIndivucialTeacher(),
-                            settings: RouteSettings(
-                              arguments: currentName,
-                            ),
-                          ));
-                    }
-                  },
-                ),
-                const Padding(padding: EdgeInsets.only(top: 10.0)),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size.fromHeight(
-                        40), // fromHeight use double.infinity as width and 40 is the height
-                  ),
-                  child: const Text(
-                    "See all students reports",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => AllReportTeacher()));
-                  },
-                ),
-              ],
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Padding(padding: EdgeInsets.only(top: 10.0)),
+            Text(
+              'School Name : $_name',
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(fontWeight: FontWeight.bold),
             ),
-          );
-        },
+            DropdownButton(
+              value: _currentClass,
+              isExpanded: true,
+              icon: const Icon(Icons.keyboard_arrow_down),
+              items: classes.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                  child: Text(items),
+                );
+              }).toList(),
+              onChanged: (String newValue) {
+                setState(() {
+                  _currentClass = newValue;
+                });
+              },
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(
+                    40), // fromHeight use double.infinity as width and 40 is the height
+              ),
+              child: Text(
+                "Add texts in $_currentClass",
+                style: const TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                //debugPrint("Butona tıklandı");
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AddTextToClassTeacher(),
+                      settings: RouteSettings(
+                        arguments: _currentClass,
+                      ),
+                    ));
+              },
+            ),
+            const Padding(padding: EdgeInsets.only(top: 10.0)),
+            Text("Texts In $_currentClass"),
+            ListView.builder(
+                shrinkWrap: true,
+                itemCount: textsInClass.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: const Icon(Icons.text_snippet, color: Colors.cyan),
+                    title: Text(textsInClass[index]),
+                    trailing: const Icon(
+                      Icons.edit,
+                      color: Colors.green,
+                    ),
+                  );
+                }),
+            Text("Students In $_currentClass"),
+            ListView.builder(
+                shrinkWrap: true,
+                itemCount: studentsInClass.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading:
+                        const Icon(Icons.people_rounded, color: Colors.teal),
+                    title: Text(studentsInClass[index]),
+                    trailing: const Icon(
+                      Icons.select_all_sharp,
+                      color: Colors.black,
+                    ),
+                    onTap: () => {
+                      currentName = studentsInClass[index],
+                      showAlertDialog(context)
+                    },
+                  );
+                }),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(
+                    40), // fromHeight use double.infinity as width and 40 is the height
+              ),
+              child: const Text(
+                "See the selected student report",
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                if (currentName == "") {
+                  showAlertDialog(context);
+                } else {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ReportIndivucialTeacher(),
+                        settings: RouteSettings(
+                          arguments: currentName,
+                        ),
+                      ));
+                }
+              },
+            ),
+            const Padding(padding: EdgeInsets.only(top: 10.0)),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(
+                    40), // fromHeight use double.infinity as width and 40 is the height
+              ),
+              child: const Text(
+                "See all students reports",
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => AllReportTeacher()));
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
