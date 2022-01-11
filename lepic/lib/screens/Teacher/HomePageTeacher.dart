8453829b29f60a416bd09318109233ee3580df2d
barisincesu <barisincesu@dreamgames.com>
2022-01-11 04:01:35 +0300

    TeacherHomePage v1 released.

--- lepic/lib/screens/Teacher/HomePageTeacher.dart
+++ lepic/lib/screens/Teacher/HomePageTeacher.dart
@@ -32,19 +32,7 @@ class TeacherHomePage extends StatefulWidget {
 String currentName = "";
 
 class _TeacherHomePage extends State<TeacherHomePage> {
-  final String _name = "Vefa Lisesi"; // Veritabanından çek !
-  String _currentClass = '10/A'; // Comboda aktif olanı seç
-
-  final classes = ["10/A", "11/B", "12/C", "12/A"];
-
-  final List<String> studentsInClass = <String>[
-    "Bariş İncesu",
-    "Muhammed Furkan Demir"
-  ];
-  final List<String> textsInClass = <String>[
-    "Text 1",
-    "Text 2",
-  ];
+  var selectedCurrency, selectedType;
   @override
   Widget build(BuildContext context) {
     final todo = ModalRoute.of(context).settings.arguments;
@@ -70,15 +58,184 @@ class _TeacherHomePage extends State<TeacherHomePage> {
                     child: CircularProgressIndicator(),
                   );
                 }
+                List<DropdownMenuItem> currencyItems = [];
+                for (int i = 0; i < snapshot.data.docs.length; i++) {
+                  DocumentSnapshot snap = snapshot.data.docs[i];
+                  currencyItems.add(
+                    DropdownMenuItem(
+                      child: Text(
+                        snap.get('name'),
+                        style: TextStyle(color: Color(0xff11b719)),
+                      ),
+                      value: "${snap.get('name')}",
+                    ),
+                  );
+                }
 
-                return ListView(
-                  shrinkWrap: true,
-                  children: snapshot.data.docs.map((document) {
-                    return Container(
-                      child: Center(child: Text(document['name'])),
-                    );
-                  }).toList(),
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(width: 50.0),
+                    DropdownButton(
+                      items: currencyItems,
+                      onChanged: (currencyValue) {
+                        final snackBar = SnackBar(
+                          content: Text(
+                            'Selected Currency value is $currencyValue',
+                            style: TextStyle(color: Color(0xff11b719)),
+                          ),
+                        );
+                        Scaffold.of(context).showSnackBar(snackBar);
+                        setState(() {
+                          selectedCurrency = currencyValue;
+                        });
+                      },
+                      value: selectedCurrency,
+                      isExpanded: false,
+                      hint: new Text(
+                        "Choose a Class",
+                        style: TextStyle(color: Color(0xff11b719)),
+                      ),
+                    ),
+                  ],
                 );
+              },
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(
+                    40), // fromHeight use double.infinity as width and 40 is the height
+              ),
+              child: Text(
+                "Add texts in $selectedCurrency",
+                style: const TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                //debugPrint("Butona tıklandı");
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AddTextToClassTeacher(),
+                      settings: RouteSettings(
+                        arguments: selectedCurrency,
+                      ),
+                    ));
+              },
+            ),
+            const Padding(padding: EdgeInsets.only(top: 10.0)),
+            Text("Texts In $selectedCurrency"),
+            StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('Sentence')
+                  .where('group', isEqualTo: selectedCurrency)
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                List<String> textsInClass = [];
+                for (int i = 0; i < snapshot.data.docs.length; i++) {
+                  DocumentSnapshot snap = snapshot.data.docs[i];
+                  textsInClass.add(snap.get('title'));
+                }
+
+                return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: textsInClass.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading:
+                            const Icon(Icons.text_snippet, color: Colors.cyan),
+                        title: Text(textsInClass[index]),
+                        trailing: const Icon(
+                          Icons.edit,
+                          color: Colors.green,
+                        ),
+                      );
+                    });
+              },
+            ),
+            Text("Students In $selectedCurrency"),
+            StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('Person')
+                  .where('group', isEqualTo: selectedCurrency)
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                List<String> studentsInClass = [];
+                for (int i = 0; i < snapshot.data.docs.length; i++) {
+                  DocumentSnapshot snap = snapshot.data.docs[i];
+                  studentsInClass.add(snap.get('userName'));
+                }
+
+                return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: studentsInClass.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: const Icon(Icons.people_rounded,
+                            color: Colors.teal),
+                        title: Text(studentsInClass[index]),
+                        trailing: const Icon(
+                          Icons.select_all_sharp,
+                          color: Colors.black,
+                        ),
+                        onTap: () => {
+                          currentName = studentsInClass[index],
+                          showAlertDialog(context)
+                        },
+                      );
+                    });
+              },
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(
+                    40), // fromHeight use double.infinity as width and 40 is the height
+              ),
+              child: const Text(
+                "See the selected student report",
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                if (currentName == "") {
+                  showAlertDialog(context);
+                } else {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ReportIndivucialTeacher(),
+                        settings: RouteSettings(
+                          arguments: currentName,
+                        ),
+                      ));
+                }
+              },
+            ),
+            const Padding(padding: EdgeInsets.only(top: 10.0)),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(
+                    40), // fromHeight use double.infinity as width and 40 is the height
+              ),
+              child: const Text(
+                "See all students reports",
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => AllReportTeacher()));
               },
             ),
           ],
