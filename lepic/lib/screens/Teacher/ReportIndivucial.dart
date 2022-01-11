6199979eed9b7a59a2055cfa6dd493d718a0c3d3
barisincesu <barisincesu@dreamgames.com>
2022-01-11 05:14:56 +0300

    Add Results.

--- lepic/lib/screens/Teacher/ReportIndivucial.dart
+++ lepic/lib/screens/Teacher/ReportIndivucial.dart
@@ -5,10 +5,11 @@ import 'package:lepic/screens/Teacher/StudentTextDetail.dart';
 // ignore: must_be_immutable
 class ReportIndivucialTeacher extends StatelessWidget {
   const ReportIndivucialTeacher({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final todo = ModalRoute.of(context).settings.arguments;
-
+    var clicked = 0;
     //final complatedText = ["Text 1", "Text 2", "Text 3", "Text 4"];
     //final complatedTextZCNO = ["0.65", "0.85", "0.75", "0.65"];
     return Scaffold(
@@ -39,9 +40,9 @@ class ReportIndivucialTeacher extends StatelessWidget {
                 for (int i = 0; i < snapshot.data.docs.length; i++) {
                   DocumentSnapshot snap = snapshot.data.docs[i];
                   complatedText.add(snap.get('sentenceTitle'));
-                  complatedTextZCNO.add(snap.get('grade')[5]);
+                  complatedTextZCNO.add(snap.get('grade')[4]);
                 }
-
+                List<String> gonderielcek = [];
                 return ListView.builder(
                     shrinkWrap: true,
                     itemCount: complatedText.length,
@@ -59,8 +60,11 @@ class ReportIndivucialTeacher extends StatelessWidget {
                               MaterialPageRoute(
                                 builder: (context) => StudentTextDetail(),
                                 settings: RouteSettings(
-                                  arguments: complatedText[index],
-                                ),
+                                    //ExtractArgumentsScreen.routeName,
+                                    arguments: (todo.toString() +
+                                            "*" +
+                                            complatedText[index])
+                                        .toString()),
                               ))
                         },
                       );
--- lepic/lib/screens/Teacher/StudentTextDetail.dart
+++ lepic/lib/screens/Teacher/StudentTextDetail.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 // ignore: must_be_immutable
 class StudentTextDetail extends StatelessWidget {
   const StudentTextDetail({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final todo = ModalRoute.of(context).settings.arguments;
@@ -13,29 +15,56 @@ class StudentTextDetail extends StatelessWidget {
       "Total Number Words Incorrectly Read",
       "Z Score"
     ];
-    final scoreText = ["02:59", "240", "250", "242", "8", "0.9"];
+    //final scoreText = ["02:59", "240", "250", "242", "8", "0.9"];
+
+    final info = todo.toString().split("*");
+    String _sn = info[0];
+    String _tn = info[1];
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: Text("Detail of $todo"),
+        title: Text("Detail of $_tn"),
       ),
       body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            ListView.builder(
-                shrinkWrap: true,
-                itemCount: titles.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: const Icon(
-                      Icons.check_circle_outline_sharp,
-                      color: Colors.green,
-                    ),
-                    title: Text(titles[index]),
-                    subtitle: Text(scoreText[index]),
+            StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('Grade')
+                  .where('studentName', isEqualTo: _sn)
+                  .where('sentenceTitle', isEqualTo: _tn)
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
                   );
-                }),
+                }
+                List<String> grades = [];
+                for (int i = 0; i < snapshot.data.docs.length; i++) {
+                  DocumentSnapshot snap = snapshot.data.docs[i];
+                  for (var j = 0; j < 4; j++) {
+                    grades.add(snap.get('grade')[j].toString());
+                  }
+                }
+                return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: titles.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: const Icon(
+                          Icons.check_circle_outline_sharp,
+                          color: Colors.green,
+                        ),
+                        title: Text(titles[index]),
+                        subtitle: Text(grades[index]),
+                      );
+                    });
+              },
+            ),
           ],
         ),
       ),
