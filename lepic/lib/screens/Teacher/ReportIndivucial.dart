c7f2bedb01d54c83cd1e6bb0aad09333347b1c7d
barisincesu <barisincesu@dreamgames.com>
2022-01-11 04:17:14 +0300

    Add text ZCNO data from Firestore.

--- lepic/lib/screens/Teacher/ReportIndivucial.dart
+++ lepic/lib/screens/Teacher/ReportIndivucial.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:lepic/screens/Teacher/StudentTextDetail.dart';
 
@@ -8,8 +9,8 @@ class ReportIndivucialTeacher extends StatelessWidget {
   Widget build(BuildContext context) {
     final todo = ModalRoute.of(context).settings.arguments;
 
-    final complatedText = ["Text 1", "Text 2", "Text 3", "Text 4"];
-    final complatedTextZCNO = ["0.65", "0.85", "0.75", "0.65"];
+    //final complatedText = ["Text 1", "Text 2", "Text 3", "Text 4"];
+    //final complatedTextZCNO = ["0.65", "0.85", "0.75", "0.65"];
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
@@ -21,29 +22,51 @@ class ReportIndivucialTeacher extends StatelessWidget {
           children: [
             const Padding(padding: EdgeInsets.only(top: 10.0)),
             Text('$todo Completed Texts'),
-            ListView.builder(
-                shrinkWrap: true,
-                itemCount: complatedText.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(complatedText[index]),
-                    subtitle: Text(complatedTextZCNO[index]),
-                    trailing: const Icon(
-                      Icons.arrow_forward_rounded,
-                      color: Colors.blueAccent,
-                    ),
-                    onTap: () => {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => StudentTextDetail(),
-                            settings: RouteSettings(
-                              arguments: complatedText[index],
-                            ),
-                          ))
-                    },
+            StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('Grade')
+                  .where('studentName', isEqualTo: todo)
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
                   );
-                }),
+                }
+                List<String> complatedText = [];
+                List<double> complatedTextZCNO = [];
+                for (int i = 0; i < snapshot.data.docs.length; i++) {
+                  DocumentSnapshot snap = snapshot.data.docs[i];
+                  complatedText.add(snap.get('sentenceTitle'));
+                  complatedTextZCNO.add(snap.get('grade')[5]);
+                }
+
+                return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: complatedText.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(complatedText[index]),
+                        subtitle: Text(complatedTextZCNO[index].toString()),
+                        trailing: const Icon(
+                          Icons.arrow_forward_rounded,
+                          color: Colors.blueAccent,
+                        ),
+                        onTap: () => {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => StudentTextDetail(),
+                                settings: RouteSettings(
+                                  arguments: complatedText[index],
+                                ),
+                              ))
+                        },
+                      );
+                    });
+              },
+            ),
           ],
         ),
       ),
